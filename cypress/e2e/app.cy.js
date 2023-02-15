6d8e4f50f88f8d7e733d5dde674bdfe100c1f0a8
Choi Eun Jeong <qhdrn67@naver.com>
2023-02-15 16:45:33 +0900

    cypress : more testing

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -19,4 +19,17 @@ describe('Youtube App', () => {
   it('shows popular videos first', () => {
     cy.findByText('Survive 100 Days In Circle, Win $500,000').should('exist');
   });
+
+  it('searches by keyword', () => {
+    cy.findByPlaceholderText('Search...').type('bts');
+    cy.findByRole('button').click();
+    cy.findByText('Search Result1').should('exist');
+  });
+
+  it('goes to video detail page', () => {
+    cy.findAllByRole('listitem').first().click();
+    cy.findByTitle('Survive 100 Days In Circle, Win $500,000').should('exist');
+    cy.findByText('Survive 100 Days In Circle, Win $500,000').should('exist');
+    cy.findByText(/(THE LAST OF US Trailer)/g).should('exist');
+  });
 });
