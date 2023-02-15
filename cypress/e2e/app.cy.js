db0e2020a2894543a1f79463d5376744a477f2ad
Choi Eun Jeong <qhdrn67@naver.com>
2023-02-15 16:20:48 +0900

    cypress intercept : api network mock testing

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,10 +2,21 @@
 
 describe('Youtube App', () => {
   beforeEach(() => {
+    cy.intercept('GET', /(mostPopular)/g, {
+      fixture: 'popular.json',
+    });
+    cy.intercept('GET', /(search)/g, {
+      fixture: 'search.json',
+    });
+    cy.viewport(1200, 800);
     cy.visit('/');
   });
 
   it('renders', () => {
     cy.findByText('Youtube').should('exist');
+  });
+
+  it('shows popular videos first', () => {
+    cy.findByText('Survive 100 Days In Circle, Win $500,000').should('exist');
   });
 });
